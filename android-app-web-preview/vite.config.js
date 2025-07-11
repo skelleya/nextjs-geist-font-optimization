61b2220cbd778d10699f033d18bd76040bc89fba
skelleya <103787338+skelleya@users.noreply.github.com>
2025-07-11 10:50:55 -0400

    Add android-app-web-preview/vite.config.js 1752245454003

---
+++ android-app-web-preview/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['dfdq3z-8000.csb.app']
+  }
+})
